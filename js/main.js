2dc11b246d2756e187d9f38204e8061ced2b668a
putn1k <skachok11@yandex.ru>
2022-11-14 16:17:30 +0300

    добавил функцию поиска случайного числа

---
+++ js/main.js
@@ -0,0 +1,11 @@
+function getRandomPositiveNumber( min, max ) {
+  if ( min < 0 || max < 0 ) {
+    return NaN;
+  }
+  const lower = Math.ceil( Math.min( min, max ) );
+  const upper = Math.floor( Math.max( min, max ) );
+
+  return Math.floor( Math.random() * ( upper - lower + 1 ) + lower );
+}
+
+getRandomPositiveNumber( 0, 5 );
